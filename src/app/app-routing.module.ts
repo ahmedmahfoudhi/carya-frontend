261ebd1c6a094e901bbde68de4d4c7f8df21c599
Ahmed <mahfoudhiahmed2000@gmail.com>
2023-01-18 00:16:34 +0100

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
 import { AddItemComponent } from './components/dashboard/add-item/add-item.component';
 import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
 import { MyItemsComponent } from './components/dashboard/my-items/my-items.component';
 import { RequestsComponent } from './components/dashboard/requests/requests.component';
+import { HomeSectionsComponent } from './components/home-sections/home-sections.component';
 import { HomeComponent } from './components/home/home.component';
+import { ItemsListComponent } from './components/items/items-list/items-list.component';
 import { OurTeamComponent } from './components/our-team/our-team.component';
 
 const routes: Routes = [
-  {path:'home',component:HomeComponent},
-  {path:'additem',component:AddItemComponent},
-  {path:'myitems',component:MyItemsComponent},
-  {path:'requests',component:RequestsComponent}
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: '', component: HomeSectionsComponent },
+      { path: 'cars', component: ItemsListComponent },
+      { path: 'houses', component: ItemsListComponent },
+    ],
+  },
+  { path: '', component: OurTeamComponent },
+  { path: '', component: ContactComponent },
+  {
+    path: 'user',
+    component: DashboardComponent,
+    children: [
+      { path: 'my-items', component: MyItemsComponent },
+      { path: 'add-item', component: AddItemComponent },
+      { path: 'requests', component: RequestsComponent },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/dashboard/side-nav/nav-data.ts
+++ src/app/components/dashboard/side-nav/nav-data.ts
@@ -1,27 +1,27 @@
 export const navbarData = [
-    {
-        routeLink :'home',
-        icon:'fal fa-home',
-        label:'Home'
-    },
-    {
-        routeLink: 'myitems',
-        icon: 'fa fa-list',
-        label: 'My Items'
-    },
-    {
-        routeLink: 'additem',
-        icon: 'fa fa-plus-circle',
-        label: 'Add an Item',
-    },
-    {
-        routeLink: 'requests',
-        icon: 'fa fa-paper-plane',
-        label: 'Requests',
-    },
-    {
-        routeLink: '',
-        icon: 'fa fa-sign-out',
-        label: 'Logout'
-    },
-]
+  {
+    routeLink: '',
+    icon: 'fal fa-home',
+    label: 'Home',
+  },
+  {
+    routeLink: 'my-items',
+    icon: 'fa fa-list',
+    label: 'My Items',
+  },
+  {
+    routeLink: 'add-item',
+    icon: 'fa fa-plus-circle',
+    label: 'Add an Item',
+  },
+  {
+    routeLink: 'requests',
+    icon: 'fa fa-paper-plane',
+    label: 'Requests',
+  },
+  {
+    routeLink: 'logout',
+    icon: 'fa fa-sign-out',
+    label: 'Logout',
+  },
+];
