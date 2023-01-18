281d9ce143ef61bd0833c8737d264987780bd8dd
Mazen Issaoui <issaouimazen@gmail.com>
2023-01-18 13:42:46 +0100

    added autologin and autologout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -12,6 +12,7 @@ import { User } from '../models/user.model';
 })
 export class AuthService {
   userSubject= new BehaviorSubject<any>(null);
+  private tokenExpirationTimer:any;
   constructor(private http: HttpClient,private router:Router) {}
 
   login(email: string, password: string) {
@@ -22,18 +23,51 @@ export class AuthService {
         password: password,
       }
     ).pipe(tap((resData=>{
-      const expirationDate =new Date(new Date().getTime() + +resData.expiresIn *1000);
-      const curUser=new currentUser(resData.email,resData.token,expirationDate);
-      this.userSubject.next(curUser); 
+      this.handleAuth(resData.email,resData.token,+resData.expiresIn);
     })));
   }
 
+  private handleAuth(email:string,token:string,expiresIn:number){
+    const expirationDate =new Date(new Date().getTime() + expiresIn *1000);
+    const curUser=new currentUser(email,token,expirationDate);
+    this.userSubject.next(curUser); 
+    this.autoLogout(expiresIn * 1000)
+    localStorage.setItem('user',JSON.stringify(curUser));
+  }
   register(user: User) {
     return this.http.post(environment.backendUrl + 'auth/register', user);
   }
 
   logout(){
     this.userSubject.next(null);
+    localStorage.clear();
     this.router.navigate(["/"]);
+    if(this.tokenExpirationTimer){
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer=null;
+  }
+  autoLogin(){
+    const user:{
+      email:string,
+      _token:string,
+      _expriresIn:string
+    }=JSON.parse(localStorage.getItem('user')!);
+    if(!user)
+    return ;
+
+    const loadedUser=new currentUser(user.email,user._token,new Date(user._expriresIn));
+    if(loadedUser.token){
+      const expirationDuration=new Date(user._expriresIn).getTime()-new Date().getTime();
+      this.autoLogout(expirationDuration);
+      this.userSubject.next(loadedUser);
+    }
+
+  }
+
+  autoLogout(expirationDate:number){
+    this.tokenExpirationTimer=setTimeout(()=>{
+      this.logout();
+    },expirationDate); 
   }
 }
